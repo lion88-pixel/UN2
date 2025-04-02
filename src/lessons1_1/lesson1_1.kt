7511864e42bb9517f7c0910fa1d17a6e8f59c39a
Александр <lion_alex1988@mail.ru>
2025-04-02 16:57:58 +0300

    UN2-1 variant

---
+++ src/lessons1_1/lesson1_1.kt
@@ -0,0 +1,8 @@
+package lessons1_1
+
+fun main() {
+    val productForSale:Int = 75
+    val thanksForThePurchase:String = "Спасибо за покупку, ждем Вас снова"
+    println(productForSale)
+    println(thanksForThePurchase)
+}
