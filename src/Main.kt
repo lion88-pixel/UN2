100015aa75e4155fbc9d8549b1e7f5a34142b6d4
Александр <lion_alex1988@mail.ru>
2025-04-02 16:58:52 +0300

    UN2-1 variant

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+
+fun main() {
+    val productForSale:Int = 75
+    val thanksForThePurchase:String = "Спасибо за покупку, ждем Вас снова"
+    println(productForSale)
+    println(thanksForThePurchase)
+}
